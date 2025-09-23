ee3798ac0c54bf7893d67e99cb4431f8d8d95df1
Renato Mello <35300051+Mellosre@users.noreply.github.com>
2025-09-23 18:21:46 -0300

    testando....

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -534,7 +534,9 @@ export const generateWAMessageContent = async (
 		}
 	} else if ('requestPhoneNumber' in message) {
 		m.requestPhoneNumberMessage = {}
-	} else {
+	} else if ('listMessage' in message && !!message.listMessage) {
+		 m = { listMessage: WAProto.Message.ListMessage.create(message.listMessage) };
+	}else {
 		m = await prepareWAMessageMedia(message, options)
 	}
 
